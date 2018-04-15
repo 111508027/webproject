2ea81f21f0221b9cd1fd565928e7324edf91de4c
Pawan Hage <hageps15.it@coep.ac.in>
2018-04-16 00:09:55 +0530

    Create en.php

---
+++ languages/en.php
@@ -0,0 +1,79 @@
+<?php
+	$lang = array(
+		"book_a_truck" => "BOOK A TRUCK",
+		"doorstep_delivery" => "Door Step Delivery",
+		"about" => "About",
+		"faq" => "FAQ",
+		"contact" => "Contact",
+		"login" => "Login",
+		"signup" => "Signup",
+		"about_book_a_truk" => "About BOOK A TRUCK",
+		"about_section1" => "BOOK A TRUCK is a unified marketplace which connects load owners, truck/fleet owners, transport intermediaries, transport companies, packers and movers and others for efficient movement of goods. With a state-of-the-art web and mobile platform, and a fast growing user base, BOOK A TRUCK is making Transportation of goods social, time-saving and convenient by providing numerous options to choose from.",		"faq" => "FAQ",
+		"about_section2" => "BOOK A TRUCK intends to organise the sector of transportation of goods by bringing the consignors and transporters together for exchange of available loads and trucks and providing the flexibility to select any available load or truck. The transporters are verified and our two way rating & review system ensures users reliability and trustworthiness.",
+		"get_in_touch" => "Get in Touch",
+		"our_values" => "OUR VALUES",
+		"mission" => "MISSION:We aim to deliver quality service at competitive price and back up every shipment with latest technology & outstanding customer service.We at BOOK A TRUCK try to support our customers for their customized requirements which are not feasible for a local transporter.",
+		"vision" => "VISION: We are proud to say that our modernistic way of truck hiring service & fleet solutions bestow the way people used to hire transport solutions and our mobile enabled technology seamlessly communicates with our immense network of truck operators to bring in a responsive value stream. Our partner operators are verified and trained to deliver a reliable & trustworthy service to achieve a higher customer satisfaction. BOOK A TRUCK is tugged in a revolutionary way by a group of technology fanatics & logistics experts having wealth of experience and a thorough knowledge of this unique platform and strive hard to serve the freight market in an efficient way. Now no more haggle or wrangle for transporting your goods and we would like say that it's time to 'Truck it Easy' with BOOK A TRUCK",
+		"portfolio" => "PORTFOLIO",
+		"what_we_have_created" => "What We have Created",
+		"what_our_customers_say" => "WHAT OUR CUSTOMERS SAY",
+		"comment1" => '"This company is the best. I am so happy with the result!"',
+		"by1" => "Michael Roe, Vice President, Comment Box",
+		"comment2" => 'One word... WOW!!"',
+		"by2" => "John Doe, Salesman, Rep Inc",
+		"comment3" => 'Could I... BE any more happy with this company?"',
+		"by3" => "Chandler Bing, Actor, FriendsAlot",
+
+		"que1" => "1. What is BOOK A TRUCK?",
+		"ans1" => "BOOK A TRUCK is a unified marketplace which connects load owners, truck/fleet owners, transport in-termediaries, transport companies, packers and movers and others for exchange of available loads and trucks and provides the flexibility to select any available load or truck.",
+		"que2" => "2. How Does BOOK A TRUCK Work?",
+		"ans2" => "BOOK A TRUCK is a two-way working platform.Consignors / Transporters posts a load / truck at the portal and gets instant matches as per the available trucks / loads already posted by other Consignors / Transporters. Consignor also has the option to contact transporters available as per the load details in surrounding areas.",
+		"que3" => "3. Who can Post a Load/Truck?",
+		"ans3" => "Load: Any Person or Entity that has a load of material to be delivered from one place to another. The Person or Entity can be the Owner of the Load or can be an Associate to the Owner of the Load. Truck: Transporters like Transport Company or Agency, Truck Owner, Fleet Owner, Truck Brokers, Packers & Movers, Business Owner that has a Truck available for movement from one place to another",
+		"que4" => "4. Why do I need to signup, and is it Free?",
+		"ans4" => "You do not need to sign-up as a user in order to search for a Truck/Load. When you find a Load/Truck that you are interested in and you want to contact the other user, you will then need to join BOOK A TRUCK (only registered users can contact other users). It is completely free, quick and easy to join BOOK A TRUCK for a Consignor. However, to register as a Transporter, some detailed information is required.",
+		"que5" => "5. How do Pricing and Payment work?",
+		"ans5" => "Prices may/may not be negotiable and are offered by Transporters when they post a available truck or accept a available load. BOOK A TRUCK allow only verified and registered Transporters on its portal which ensures the prices are competitive. Terms of Payments is decided between the Consignor and Transporter as per their mutual agree-ment. BOOK A TRUCK is not involved in any payment transaction.",
+		"que6" => "6. Does BOOK A TRUCK provide guarantee accuracy, timelines, safety and completeness of the transaction?",
+		"ans6" => "BOOK A TRUCK is a unified marketplace connecting different types of goods transporters and consignors to carry a transaction as per mutually agreed terms & conditions. We do not guarantee accuracy, timelines, safety and completeness of the transaction. However, it is our endeavour to bring only the verified and trusted users on our portal.",
+	
+
+		"contact_info" => "Contact us and we'll get back to you within 24 hours.",
+		"faq" => "FAQ",
+		"faq" => "FAQ",
+		"faq" => "FAQ",
+		"faq" => "FAQ",
+		"faq" => "FAQ",
+		"faq" => "FAQ",
+		"faq" => "FAQ",
+		"description" => "This is explanation about my amazing website!",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		"lang_en" => "English",
+		"lang_mr" => "Marathi",
+		"lang_hn" => "Hindi"
+	);
+?>
