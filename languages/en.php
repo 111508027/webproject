d8717cf2605e69f9f4e9d5e74388bea30e8da174
Pawan Hage <hageps15.it@coep.ac.in>
2018-04-17 11:30:55 +0530

    Add files via upload
    Langauge files are added

--- languages/en.php
+++ languages/en.php
@@ -7,13 +7,17 @@
 		"contact" => "Contact",
 		"login" => "Login",
 		"signup" => "Signup",
-		"about_book_a_truk" => "About BOOK A TRUCK",
+		'home' => 'Home',
+		'logout' => 'Logout',
+		"about_book_a_truck" => "About BOOK A TRUCK",
 		"about_section1" => "BOOK A TRUCK is a unified marketplace which connects load owners, truck/fleet owners, transport intermediaries, transport companies, packers and movers and others for efficient movement of goods. With a state-of-the-art web and mobile platform, and a fast growing user base, BOOK A TRUCK is making Transportation of goods social, time-saving and convenient by providing numerous options to choose from.",		"faq" => "FAQ",
 		"about_section2" => "BOOK A TRUCK intends to organise the sector of transportation of goods by bringing the consignors and transporters together for exchange of available loads and trucks and providing the flexibility to select any available load or truck. The transporters are verified and our two way rating & review system ensures users reliability and trustworthiness.",
 		"get_in_touch" => "Get in Touch",
 		"our_values" => "OUR VALUES",
-		"mission" => "MISSION:We aim to deliver quality service at competitive price and back up every shipment with latest technology & outstanding customer service.We at BOOK A TRUCK try to support our customers for their customized requirements which are not feasible for a local transporter.",
-		"vision" => "VISION: We are proud to say that our modernistic way of truck hiring service & fleet solutions bestow the way people used to hire transport solutions and our mobile enabled technology seamlessly communicates with our immense network of truck operators to bring in a responsive value stream. Our partner operators are verified and trained to deliver a reliable & trustworthy service to achieve a higher customer satisfaction. BOOK A TRUCK is tugged in a revolutionary way by a group of technology fanatics & logistics experts having wealth of experience and a thorough knowledge of this unique platform and strive hard to serve the freight market in an efficient way. Now no more haggle or wrangle for transporting your goods and we would like say that it's time to 'Truck it Easy' with BOOK A TRUCK",
+		"mission" => "MISSION:",
+		'mission_section' => 'We aim to deliver quality service at competitive price and back up every shipment with latest technology & outstanding customer service.We at BOOK A TRUCK try to support our customers for their customized requirements which are not feasible for a local transporter.',
+		"vision" => "VISION:",
+		'vision_section' => 'We are proud to say that our modernistic way of truck hiring service & fleet solutions bestow the way people used to hire transport solutions and our mobile enabled technology seamlessly communicates with our immense network of truck operators to bring in a responsive value stream. Our partner operators are verified and trained to deliver a reliable & trustworthy service to achieve a higher customer satisfaction. BOOK A TRUCK is tugged in a revolutionary way by a group of technology fanatics & logistics experts having wealth of experience and a thorough knowledge of this unique platform and strive hard to serve the freight market in an efficient way. Now no more haggle or wrangle for transporting your goods and we would like say that its time to "Truck it Easy" with BOOK A TRUCK',
 		"portfolio" => "PORTFOLIO",
 		"what_we_have_created" => "What We have Created",
 		"what_our_customers_say" => "WHAT OUR CUSTOMERS SAY",
@@ -36,41 +40,60 @@
 		"ans5" => "Prices may/may not be negotiable and are offered by Transporters when they post a available truck or accept a available load. BOOK A TRUCK allow only verified and registered Transporters on its portal which ensures the prices are competitive. Terms of Payments is decided between the Consignor and Transporter as per their mutual agree-ment. BOOK A TRUCK is not involved in any payment transaction.",
 		"que6" => "6. Does BOOK A TRUCK provide guarantee accuracy, timelines, safety and completeness of the transaction?",
 		"ans6" => "BOOK A TRUCK is a unified marketplace connecting different types of goods transporters and consignors to carry a transaction as per mutually agreed terms & conditions. We do not guarantee accuracy, timelines, safety and completeness of the transaction. However, it is our endeavour to bring only the verified and trusted users on our portal.",
-	
-
 		"contact_info" => "Contact us and we'll get back to you within 24 hours.",
-		"faq" => "FAQ",
-		"faq" => "FAQ",
-		"faq" => "FAQ",
-		"faq" => "FAQ",
-		"faq" => "FAQ",
-		"faq" => "FAQ",
-		"faq" => "FAQ",
 		"description" => "This is explanation about my amazing website!",
+		'payment' => 'Payment',
+		'calculate_rate' => 'Calculate Rate',
+		'placeorder' => 'Place Your Order',
+		'bills' => 'Bills',
+		'vehicle' => 'Vehicle',
+		'driver' => 'Driver',
+		'city' => 'City',
+		'mobile_Number' => 'Mobile No',
+		'generte_Bill' => 'Generate Bill',
+		'show_Order' => 'Show Order',
+		'name' => 'Name',
+		'email' => 'Email',
+		'urgent_delivery_needed' => 'Urgent Delivery Needed',
+		'drivers_database' => 'Driver Database',
+		'vehicles_database' => 'Vehicle Database',
+		'pincode' => 'Pin Code',
+		'for_orders_and_bills' => 'For Orders and Bills',
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		'you_can_add_delete_update_the_info' => 'You can Add, Delete, Update the Information',
+		'maintain_vehicle' => "Maintain the Vehicle's Database",
+		'maintain_driver' => "Maintain the Driver's Database",
+		
+		"veh_sec" => "Vehicle Database Contain the Following Info",
+		"driver_sec" => "Driver Database Contain the Following Info",
+		
+		'veh_type' => 'Vehicle Type',
+		'veh_name' => 'Vehicle Name',
+		'veh_reg_no' => 'Vehicle Register No',
+		'loadcapacity' => 'Loadcapacity',
+		
+		'driver_type' => 'Driver Type',
+		'driver_name' => 'Driver Name',
+		'driver_lic' => 'Driver License No.',
+		'driver_age' => 'Driver Age',
+		'driver_city' => 'Driver City',
+		
+		'truck' => 'Truck',
+		'tempo' => 'Tempo',
+		'mini_truck' => 'MiniTruck',
+		'choose_vehicle' => 'Choose Vehicle',
+		'type_of_goods' => 'Type Of Goods',
+		'quantity' => 'Quantity',
+		'delivery_address' => 'Delivery Address',
+		'source' => 'Source',
+		'destination' => 'Destination',
+		'pune' => 'Pune',
+		'aurangabad' => 'Aurangabad',
+		'mumbai' => 'Mumbai',
+		'punesec' => 'Yes, We are in Pune',
+		'aurasec' => 'Yes, Aurangabad is ours',
+		'mumbaisec' => 'We reach to Mumbai',
+ 		'logged_in' => 'You are logged in',
 
 		"lang_en" => "English",
 		"lang_mr" => "Marathi",
